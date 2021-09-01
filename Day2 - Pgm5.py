3b5776c5f58a9be74d761e39809b7b983ee3b36e
Sonika-dot-Prakash <89075668+Sonika-dot-Prakash@users.noreply.github.com>
2021-09-01 11:48:53 +0530

    Update Day2 - Pgm5.py

--- Day2 - Pgm5.py
+++ Day2 - Pgm5.py
@@ -1,21 +1,15 @@
 # to create all possible strings by using 'a', 'e', 'i', 'o', 'u'.
 
-import random
 
-
-def get_perms(chars):
-    res = [''.join(chars)]
-    while len(res) != 120:
-        random.shuffle(chars)
-        s = ''.join(chars)
-        if s in res:
-            continue
-        else:
-            res.append(s)
-    return res
+def get_perms(chars, l, r):
+    if l == r:
+        print(''.join(chars), end=' ')
+    else:
+        for i in range(l, r + 1):
+            chars[i], chars[l] = chars[l], chars[i]
+            get_perms(chars, l + 1, r)
+            chars[i], chars[l] = chars[l], chars[i]
 
 
 if __name__ == "__main__":
-    perms = get_perms(['a', 'e', 'i', 'o', 'u'])
-    print("The possible strings are:")
-    print(*perms, sep=", ")
+    get_perms(['a', 'e', 'i', 'o', 'u'], 0, 4)
